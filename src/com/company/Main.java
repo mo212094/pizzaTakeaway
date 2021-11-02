e47e15bf5655a0a12ffa953fa70ca84e621ae593
Maeve O'Shea <MO212094@reigate.ac.uk>
2021-11-02 16:13:39 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	    double pizzaPrice = 0;
+        int placeholder = 0;
+        String topping;
+        Scanner input = new Scanner(System.in);
+        System.out.println("What size would you like?");
+        String pizzaSize = input.next();
+        System.out.println("How many toppings would you like?");
+        int toppings = input.nextInt();
+
+        String[] toppingsArray;
+        for () {
+            System.out.println("What topping do you want");
+            toppingsArray[i] = {};
+        }
+
+        System.out.println("The price of you pizza is: £" + );
+    }
+}
